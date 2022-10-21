5502cd2bf5e49e604ee066744a908910ebd75d95
Lab User <lab-user@ip-172-31-13-212.ap-south-1.compute.internal>
2022-10-21 15:43:23 +0530

    removed extra file

--- BankAccountManagementSystem/src/main/java/com/project/main/config/SecurityConfig.java
+++ BankAccountManagementSystem/src/main/java/com/project/main/config/SecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
 	}
+	
 }
