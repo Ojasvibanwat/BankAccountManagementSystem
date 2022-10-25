800fe7348e8195b8c36ef2eb8c9c3de67c7927e7
Lab User <lab-user@ip-172-31-13-212.ap-south-1.compute.internal>
2022-10-25 15:21:57 +0530

    BAMSTrial

---
+++ BAMSTrial/BAMSTrial/src/main/java/com/project/BAMSTrial/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.project.BAMSTrial.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.BAMSTrial.model.Client;
+
+public interface ClientRepository extends CrudRepository<Client, Integer> {
+
+}
---
+++ BAMSTrial/BAMSTrial/src/test/java/com/project/BAMSTrial/BamsTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BAMSTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamsTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
