7dc5f688363d051a28a10652106b605cd0645d52
Lab User <lab-user@ip-172-31-15-58.ap-south-1.compute.internal>
2022-10-26 12:52:01 +0530

    Documented

--- Database_Script/Sql_script.sql
+++ Database_Script/Sql_script.sql
@@ -2,18 +2,17 @@ create database bank;
 
 use bank;
 
-CREATE TABLE clients(Id INT AUTO_INCREMENT,Name varchar(100),AccountNumber INT,Balance INT,OutstandingAmount INT,DateOfJoining DATE,Password varchar(255),PRIMARY KEY (Id));
+CREATE TABLE clients(Id INT AUTO_INCREMENT,Name varchar(100),AccountNumber INT,Balance INT,
+OutstandingAmount INT,DateOfJoining DATE,Password varchar(255),PRIMARY KEY (Id));
 
-INSERT INTO clients VALUES(1,'Mark',23456,100000,100000,'2022-10-22',MD5('mark123'));
-
-INSERT INTO clients VALUES(2,'Robin',23478,200000,200000,'2022-10-22',MD5('robin123'));
+INSERT INTO clients VALUES(2,23478,200000,'2022-10-22','Robin',200000,MD5('robin123'));		   
+		    
+INSERT INTO clients VALUES(1,23456,100000,'2022-10-22','Mark',100000,MD5('mark123'));    
 
 
  -----------------------------------------------------------------------------------------------------
 
-		   
-		    
-                   
+           
 		   
 
 
