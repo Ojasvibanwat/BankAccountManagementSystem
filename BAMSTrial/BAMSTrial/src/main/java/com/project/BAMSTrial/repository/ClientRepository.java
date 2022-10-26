6b63c82fd6a458bce35a48601cb6acf8ce5476e5
Lab User <lab-user@ip-172-31-13-212.ap-south-1.compute.internal>
2022-10-26 16:38:49 +0530

    Cleaned

--- BAMSTrial/BAMSTrial/src/main/java/com/project/BAMSTrial/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.BAMSTrial.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.BAMSTrial.model.Client;
-
-public interface ClientRepository extends CrudRepository<Client, Integer> {
-
-}
